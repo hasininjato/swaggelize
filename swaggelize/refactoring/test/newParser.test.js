f9b4e31ac0278a8c1486c9324ee72f4469c5040c
rojovao <hrambonimanana@gmail.com>
2025-04-12 21:52:19 +0300

    test new parser ongoing

--- swaggelize/refactoring/test/newParser.test.js
+++ swaggelize/refactoring/test/newParser.test.js
@@ -7,11 +7,11 @@ const {
 } = require("../src/parsers/newParser");
 
 // data models
-const {profile} = require('./data/profile.model');
-const {post} = require('./data/post.model');
-const {user} = require('./data/user.model');
-const {instrument} = require('./data/instrument.model');
-const {postTag} = require('./data/post.tag.model');
+const { profile } = require('./data/profile.model');
+const { post } = require('./data/post.model');
+const { user } = require('./data/user.model');
+const { instrument } = require('./data/instrument.model');
+const { postTag } = require('./data/post.tag.model');
 
 // extract AST
 const profileAst = extractAst(profile);
@@ -20,6 +20,7 @@ const userAst = extractAst(user);
 const instrumentAst = extractAst(instrument);
 const postTagAst = extractAst(postTag);
 
+// ast traversed
 const profileModel = traverseAst(profileAst)
 const postModel = traverseAst(postAst);
 const userModel = traverseAst(userAst);
@@ -44,63 +45,32 @@ describe('model parser module', () => {
         postModel.forEach((element) => {
             expect(extractModelName(element)).toBe('Post');
         });
+        userModel.forEach((element) => {
+            expect(extractModelName(element)).toBe('User');
+        });
     });
 
-    it('extract sequelize model fields', () => {
+    it('extract model fields without sequelize', () => {
         postModel.forEach((element) => {
             expect(extractFields(element)).toStrictEqual(modelFieldsExpectedResult)
-        })
+        });
     });
 
-    it('extract sequelize model fields without timestamps', () => {
+    it('extract timestamps fields timestamps option is false', () => {
         postModel.forEach((element) => {
-            expect(extractTimestampFields(element)).not.toBe([])
+            expect(extractTimestampFields(element)).toStrictEqual([])
         })
     })
 
-    it('extract sequelize model fields with timestamps', () => {
+    it('extract timestamps fields timestamps option is true', () => {
         profileModel.forEach((element) => {
             expect(extractTimestampFields(element)).toStrictEqual(modelFieldsWithTimeStampsExpectedResult)
         })
-    });
-
-    it('extract model parser', () => {
-        expect(modelParser(post)).toStrictEqual(modelParserExpectedResult);
     })
 
     it('extract model without relation', () => {
         userModel.forEach((element) => {
             expect(extractRelations(element)).toStrictEqual({"relations": []})
         })
-    })
-
-    it('extract model with one to one relation', () => {
-        profileModel.forEach((element) => {
-            expect(extractRelations(element)).toStrictEqual(modelOneToOneRelationExpectedResult)
-        })
-    })
-
-    it('extract model with one to many relation', () => {
-        postModel.forEach((element) => {
-            expect(extractRelations(element)).toStrictEqual(modelOneToMannyRelationExpectedResult)
-        })
-    })
-
-    it('extract model with many to many relation without creating new model through', () => {
-        instrumentModel.forEach((element) => {
-            expect(extractRelations(element)).toStrictEqual(modelManyToMannyRelationThroughIsStringExpectedResult)
-        })
-    })
-
-    it('extract through relation with fields through relation is string', () => {
-        expect(extractThroughRelationWithFields(instrumentAst)).toStrictEqual(manyToMannyRelationThroughIsStringExpectedResult)
-    })
-
-    it('extract through relation with fields through relation is object', () => {
-        expect(extractThroughRelationWithFields(postTagAst)).toStrictEqual([])
-    })
-
-    it('create through model through relation is string', () => {
-        expect(createThroughModelIfString(extractThroughRelationWithFields(instrumentAst))).toStrictEqual(newModelThroughIsString)
     })
 });
